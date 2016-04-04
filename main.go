d6eeec90618acc96fdc96c186e16d4c5342666f7
Jesus Moreno <jesusrmor94@gmail.com>
2016-04-04 17:25:48 -0400

    Avoid seeding global random

--- main.go
+++ main.go
@@ -3,15 +3,48 @@ package squid
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"time"
 )
 
+var random *rand.Rand // Private random
+
+func init() {
+	random = rand.New(
+		&lockedRandSource{
+			src: rand.NewSource(time.Now().UnixNano()),
+		},
+	)
+}
+
+// locked to prevent concurrent use of the underlying source
+type lockedRandSource struct {
+	lock sync.Mutex // protects src
+	src  rand.Source
+}
+
+// to satisfy rand.Source interface
+func (r *lockedRandSource) Int63() int64 {
+	r.lock.Lock()
+	ret := r.src.Int63()
+	r.lock.Unlock()
+	return ret
+}
+
+// to satisfy rand.Source interface
+func (r *lockedRandSource) Seed(seed int64) {
+	r.lock.Lock()
+	r.src.Seed(seed)
+	r.lock.Unlock()
+}
+
 func randomItem(list []string) string {
-	return list[rand.Intn(len(list))]
+	return list[random.Intn(len(list))]
 }
 
 // GenerateID generates a new Asana style ID
 func GenerateID() string {
-	count := rand.Intn(1000) + 2
+	count := random.Intn(1000) + 2
 	firstAdjective := randomItem(adjectives)
 	secondAdjective := randomItem(adjectives)
 	noun := randomItem(nouns)
